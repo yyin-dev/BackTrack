431d5b1e95cced9464f9f93b4b23c5b83ab9ef1f
meglc23 <meglc23@connect.hku.hk>
2019-11-07 21:47:12 +0800

    nav bar, sprint page

--- frontend/ui/src/components/common/sidebar.js
+++ frontend/ui/src/components/common/sidebar.js
@@ -3,6 +3,8 @@ import { } from 'antd';
 
 import { Layout, Menu, Icon } from 'antd';
 import { Link } from "react-router-dom";
+import './sidebar.css';
+
 
 import logo from '../../assets/logo.svg'
 const { Sider } = Layout;
@@ -14,35 +16,31 @@ class Sidebar extends React.Component {
             <Sider
                 breakpoint="lg"
                 collapsedWidth="0"
-                theme="light"
+                theme="dark"
             >
                 <div className="logo">
                     <img src={logo} alt="fireSpot" />
                 </div>
-                <Menu theme="light" mode="inline" defaultSelectedKeys={['1']}>
-                    <Menu.Item key="1">
+                <Menu theme="dark" mode="inline" defaultSelectedKeys={['2']}>
+                    <Menu.Item key="1" to="/">
                         <Icon type="home" />
-                        <span className="nav-text" >
-                            <Link to="/">Home</Link>
-                        </span>
+                            <span className="nav-text" >Home</span>
+                            <Link to="/" />
                     </Menu.Item>
                     <Menu.Item key="2">
-                        <Icon type="user" />
-                        <span className="nav-text" >
-                            <Link to="/product">Product Backlog</Link>
-                        </span>
+                        <Icon type="user" />                   
+                            <span className="nav-text" >Product Backlog</span>
+                            <Link to="/product"></Link>
                     </Menu.Item>
                     <Menu.Item key="3">
                         <Icon type="video-camera" />
-                        <span className="nav-text" >
-                            <Link to="/sprint">Sprint Backlog</Link>
-                        </span>
+                            <span className="nav-text" >Sprint Backlog</span>
+                            <Link to="/sprint"></Link>
                     </Menu.Item>
                     <Menu.Item key="4">
-                        <Icon type="user" />
-                        <span className="nav-text" >
-                            <Link to="/user">User Center</Link>
-                        </span>
+                        <Icon type="user" />           
+                            <span className="nav-text" >User Center</span>
+                            <Link to="/user"></Link>
                     </Menu.Item>
                 </Menu>
             </Sider>
--- frontend/ui/src/components/sprint/sprintBacklog.js
+++ frontend/ui/src/components/sprint/sprintBacklog.js
@@ -1,28 +1,86 @@
 import React from "react";
 import {} from "antd";
 
-import { Layout, Menu, Icon, Table, Select, Radio, Button } from 'antd';
+import { Layout, Table, Tag } from "antd";
 
-const { Header, Content, Footer, Sider } = Layout;
+const { Column, ColumnGroup } = Table;
+const { Header } = Layout;
 
 class SprintBacklog extends React.Component {
-  columns = [
-    { title: "Title", dataIndex: "title", width: "10%" },
-    { title: "Sprint No", dataIndex: "sprint_no", width: "10%" },
-    { title: "Status", dataIndex: "status", width: "10%" },
-    { title: "Detail", dataIndex: "detail", width: "15%" },
-    { title: "Story Point", dataIndex: "story_point", width: "10%" },
-    { title: "Accumulated Story Point", dataIndex: "acc", width: "10%" },
-    { title: "Priority", dataIndex: "priority", width: "10%" },
+
+  data = [
+    {
+      pbi: "test1",
+      to_do: ["todo1", "todo2", "todo3", "todo4"],
+      in_progress: [
+        "inprogress1",
+        "inprogress2",
+        "inprogress3",
+        "inprogress4",
+        "inprogress5",
+        "inprogress6"
+      ],
+      done: ["done1", "done2", "done3", "done4"],
+      remain_effort: "test1",
+      total_effort: "test1"
+    }
   ];
 
   render() {
     return (
       <Layout style={{ height: "100vh" }}>
-        <Header style={{ background: '#fff', padding: 0, textAlign: "center" }}>Sprint Backlog</Header>
-        <Table
-          columns={this.columns}
-        />
+        <Header style={{ background: "#fff", padding: 0, textAlign: "center" }}>
+          Sprint Backlog
+        </Header>
+        <Table dataSource={this.data}>
+          <Column title="PBI" dataIndex="pbi" key="pbi" width="15%" />
+          <ColumnGroup title="Task">
+            <Column
+              title="To Do"
+              dataIndex="to_do"
+              key="to_do"
+              render={tags => (
+                <span>
+                  {tags.map(tag => (
+                    <Tag color="blue" key={tag}>
+                      {tag}
+                    </Tag>
+                  ))}
+                </span>
+              )}
+            />
+            <Column
+              title="In Progress"
+              dataIndex="in_progress"
+              key="in_progress"
+              render={tags => (
+                <span>
+                  {tags.map(tag => (
+                    <Tag color="blue" key={tag}>
+                      {tag}
+                    </Tag>
+                  ))}
+                </span>
+              )}
+            />
+            <Column
+              title="Done"
+              dataIndex="done"
+              key="done"
+              render={tags => (
+                <span>
+                  {tags.map(tag => (
+                    <Tag color="blue" key={tag}>
+                      {tag}
+                    </Tag>
+                  ))}
+                </span>
+              )}
+            />
+          </ColumnGroup>
+          <Column title="Remaining Effort" dataIndex="remain_effort" key="remain_effort" width="15%" />
+          <Column title="Total Effort" dataIndex="total_effort" key="total_effort" width="15%" />
+        </Table>
       </Layout>
     );
   }
