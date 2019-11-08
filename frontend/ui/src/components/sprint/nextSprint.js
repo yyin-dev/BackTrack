8780b355769ad507c1815a52b4455c3f8922506b
Fred <yy0125@connect.hku.hk>
2019-11-08 23:11:18 +0800

    Next sprint basic

--- frontend/ui/src/components/sprint/nextSprint.js
+++ frontend/ui/src/components/sprint/nextSprint.js
@@ -1,16 +1,8 @@
 import React from 'react';
 
-import { Modal, Button, Form, InputNumber, Card } from 'antd';
-
-class UnfinishedPBI extends React.Component {
-  render() {
-    return (<Card title="Default size card" extra={<a href="#">More</a>} style={{ width: 300 }}>
-      <p>Card content</p>
-      <p>Card content</p>
-      <p>Card content</p>
-    </Card>)
-  }
-}
+import { Modal, Button, Form, InputNumber, Card, Select, Input } from 'antd';
+import './nextSprint.css'
+const { Option } = Select;
 
 class NextSprint extends React.Component {
   constructor(props) {
@@ -21,8 +13,10 @@ class NextSprint extends React.Component {
     };
   }
 
-  showModal = () => {
-    this.setState({
+  // https://stackoverflow.com/a/53964245/9057530
+  showModal = async () => {
+    await this.setState({
+      unfinished_pbis: [],
       visible: true
     });
 
@@ -54,8 +48,33 @@ class NextSprint extends React.Component {
     });
   };
 
-  handleMaxCapacityInput = (e) => {
+  handleMaxCapacityInput = (e) => { }
 
+  handleActionChange = (v, index) => {
+    let newState = this.state.unfinished_pbis
+    newState[index].action = v
+    this.setState({
+      unfinished_pbis: newState
+    })
+    console.log(this.state.unfinished_pbis)
+  }
+
+  handleNewTitleChange = (v, index) => {
+    let newState = this.state.unfinished_pbis
+    newState[index].newTitle = v
+    this.setState({
+      unfinished_pbis: newState
+    })
+    console.log(this.state.unfinished_pbis)
+  }
+
+  handleNewStoryPointChange = (v, index) => {
+    let newState = this.state.unfinished_pbis
+    newState[index].newStoryPoint = v
+    this.setState({
+      unfinished_pbis: newState
+    })
+    console.log(this.state.unfinished_pbis)
   }
 
   render() {
@@ -75,17 +94,46 @@ class NextSprint extends React.Component {
         <Button type="primary" onClick={this.showModal}>
           Next Sprint
         </Button>
-        <div>
-
-
-        </div>
         <Modal
           title="Next Sprint"
           visible={this.state.visible}
           onOk={this.handleOk}
           onCancel={this.handleCancel}
         >
-          {this.state.unfinished_pbis.map(pbi => (<UnfinishedPBI />))}
+          {this.state.unfinished_pbis.map((pbi, index) =>
+            (<Card
+              className="unfinished-pbi-card"
+              title={pbi.title}
+            >
+              <span>Total effort: {pbi.total}</span><br />
+              <span>Remaining effort: {pbi.remaining}</span><br />
+              <span>Select an action:
+                <Select
+                  defaultValue="next"
+                  style={{ width: 200, paddingLeft: 10 }}
+                  onChange={v => this.handleActionChange(v, index)}
+                >
+                  <Option value="next">Move to next sprint</Option>
+                  <Option value="back">Back to product backlog</Option>
+                </Select>
+              </span><br />
+              <span>Specify new info</span><br />
+              <span>New PBI Title:
+                <Input 
+                  style={{ width: 'auto' }} 
+                  defaultValue={pbi.title} 
+                  onChange={v => this.handleNewTitleChange(v, index)}
+                />
+              </span><br />
+              <span>New PBI Story Point:
+                <InputNumber
+                  min={1}
+                  defaultValue={pbi.story_point}
+                  onChange={v => this.handleNewStoryPointChange(v, index)} 
+                />
+              </span><br />
+            </Card>))}
+
           <Form {...formItemLayout}>
             <Form.Item label="Sprint Number">
               <InputNumber
