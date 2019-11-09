ce627203bd765926f1e1edf93e37381d6b4707ad
Fred <yy0125@connect.hku.hk>
2019-11-09 10:15:35 +0800

    fix conflict

--- backend/backtrack/product/api/urls.py
+++ backend/backtrack/product/api/urls.py
@@ -24,15 +24,7 @@ urlpatterns = [
     path('<pk>/update/', PBIUpdateView.as_view()),
     path('<pk>/delete/', deletePBI.as_view()),
     path('<pk>/movebackPBI/', movebackPBI.as_view()),
-<<<<<<< HEAD
-<<<<<<< HEAD
     path('<pk>/movetonextsprint/', moveToNextSprint.as_view()),
     path('<pk>/movebackPBIaftersprint/', movebackPBIAfterSprint.as_view()),
     path('<pk>/getPBIInfo/', getPBIInfo.as_view()),
-=======
-    # path('<pk>/getPBIInfo/', getPBIInfo.as_view()),
->>>>>>> forget to delete useless code
-=======
-    # path('<pk>/getPBIInfo/', getPBIInfo.as_view()),
->>>>>>> c87b95608bfe70384e817410d482e0a64924ac7b
 ]
