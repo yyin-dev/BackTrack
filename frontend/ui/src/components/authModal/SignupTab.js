fd35bdd4b8e5de7f74e2da73547a2c0de6539339
Frankaka <cgy86189@GMAIL.COM>
2019-11-27 17:21:20 +0800

    fix bug for double log in

--- frontend/ui/src/components/authModal/SignupTab.js
+++ frontend/ui/src/components/authModal/SignupTab.js
@@ -1,106 +1,151 @@
-import React from 'react'
-import { message, Button, Form, Input, Select } from 'antd'
+import React from 'react';
+import {message, Button, Form, Input, Select} from 'antd';
 import axios from 'axios';
 
-const { Option } = Select;
+const {Option} = Select;
 
 class RegistrationForm extends React.Component {
   state = {
     confirmDirty: false,
-    loading: false
+    loading: false,
   };
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    let values = this.props.form.getFieldsValue()
-    axios.post(`http://127.0.0.1:8000/user/api/signup/`, {
-      username: values.username,
-      password: values.password,
-      role: values.role
-    })
-      .then(res => {
-        message.success("You have signed up! Please log in!", 3)
-        this.props.switchToLogin()
-      })
-      .catch(err => {
-        console.log(err)
-      })
-  }
+  handleSubmit = e => {
+    // TODO: need better solution
+    e.preventDefault ();
+    let values = this.props.form.getFieldsValue ();
+    let users = [], allUserNames = [];
+
+    axios.get (`http://127.0.0.1:8000/user/api/`).then (res => {
+      users = res.data;
+      allUserNames = users.map (user => user.username);
+
+      if (allUserNames.length > 0) {
+        if (allUserNames.includes (values.username)) {
+          message.error ('User existed. Please change a username.');
+          return;
+        }
+        else{
+          axios
+          .post (`http://127.0.0.1:8000/user/api/signup/`, {
+            username: values.username,
+            password: values.password,
+            role: values.role,
+          })
+          .then (res => {
+            message.success ('You have signed up! Please log in!', 3);
+            this.props.switchToLogin ();
+          })
+          .catch (err => {
+            console.log (err);
+          });
+        }
+      } else {
+        axios
+          .post (`http://127.0.0.1:8000/user/api/signup/`, {
+            username: values.username,
+            password: values.password,
+            role: values.role,
+          })
+          .then (res => {
+            message.success ('You have signed up! Please log in!', 3);
+            this.props.switchToLogin ();
+          })
+          .catch (err => {
+            console.log (err);
+          });
+      }
+    });
+  };
+
 
   compareToFirstPassword = (rule, value, callback) => {
     const form = this.props.form;
-    if (value && value !== form.getFieldValue('password')) {
-      callback('Different');
+    if (value && value !== form.getFieldValue ('password')) {
+      callback ('Different');
     } else {
-      callback();
+      callback ();
     }
-  }
+  };
 
   validateToNextPassword = (rule, value, callback) => {
     const form = this.props.form;
     if (value && this.state.confirmDirty) {
-      form.validateFields(['confirm'], { force: true });
+      form.validateFields (['confirm'], {force: true});
     }
-    callback();
-  }
+    callback ();
+  };
 
-  render() {
-    const { getFieldDecorator } = this.props.form;
+  render () {
+    const {getFieldDecorator} = this.props.form;
 
     const formItemLayout = {
-      labelCol: { xs: { span: 8, offset: 0 }, },
-      wrapperCol: { xs: { span: 16 }, },
+      labelCol: {xs: {span: 8, offset: 0}},
+      wrapperCol: {xs: {span: 16}},
     };
 
     const tailFormItemLayout = {
-      wrapperCol: { xs: { span: 16, offset: 8 }, },
+      wrapperCol: {xs: {span: 16, offset: 8}},
     };
 
     return (
       <Form onSubmit={this.handleSubmit} {...formItemLayout}>
-        <Form.Item label="Username" >
-          {getFieldDecorator('username', {
+        <Form.Item label="Username">
+          {getFieldDecorator ('username', {
             rules: [
-              { required: true, message: 'Enter username!' },
-              { whitespace: true, message: 'Enter username!' },
+              {required: true, message: 'Enter username!'},
+              {whitespace: true, message: 'Enter username!'},
             ],
-          })(<Input />)}
+          }) (<Input />)}
         </Form.Item>
         <Form.Item label="Password">
-          {getFieldDecorator('password', {
+          {getFieldDecorator ('password', {
             rules: [
-              { required: true, message: 'Enter password!', },
-              { validator: this.validateToNextPassword, },
-              { whitespace: true, message: 'Enter password!' }
+              {required: true, message: 'Enter password!'},
+              {validator: this.validateToNextPassword},
+              {whitespace: true, message: 'Enter password!'},
             ],
-          })(<Input type="password" />)}
+          }) (<Input type="password" />)}
         </Form.Item>
-        <Form.Item label="Confirm password" >
-          {getFieldDecorator('confirm', {
+        <Form.Item label="Confirm password">
+          {getFieldDecorator ('confirm', {
             rules: [
-              { required: true, message: 'Enter password!', },
-              { validator: this.compareToFirstPassword, }
+              {required: true, message: 'Enter password!'},
+              {validator: this.compareToFirstPassword},
             ],
-          })(<Input type="password" />)}
+          }) (<Input type="password" />)}
         </Form.Item>
 
         <Form.Item label="Role">
-          {getFieldDecorator('role', { initialValue: "Developer/Product Owner" })(
-            <Select >
+          {getFieldDecorator ('role', {
+            initialValue: 'Developer/Product Owner',
+          }) (
+            <Select>
               <Option value="Scrum Master">Scrum Master</Option>
-              <Option value="Developer/Product Owner">Developer/Product Owner</Option>
+              <Option value="Developer/Product Owner">
+                Developer/Product Owner
+              </Option>
             </Select>
           )}
         </Form.Item>
 
-        <Form.Item {...tailFormItemLayout} >
-          <Button type="primary" htmlType="submit" style={{ width: '100%' }} loading={this.state.loading}>Sign up</Button>
+        <Form.Item {...tailFormItemLayout}>
+          <Button
+            type="primary"
+            htmlType="submit"
+            style={{width: '100%'}}
+            loading={this.state.loading}
+          >
+            Sign up
+          </Button>
         </Form.Item>
       </Form>
     );
   }
 }
 
-const WrappedRegistrationForm = Form.create({ name: 'register' })(RegistrationForm);
+const WrappedRegistrationForm = Form.create ({name: 'register'}) (
+  RegistrationForm
+);
 
 export default WrappedRegistrationForm;
